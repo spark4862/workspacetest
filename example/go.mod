a0a3fd3bec27858cdc803480498a256c20c225ad
spark4862 <1323237621@qq.com>
2025-05-19 10:07:51 +0800

    init

---
+++ example/go.mod
@@ -0,0 +1,3 @@
+module github.com/spark4862/workspacetest/example
+
+go 1.24.3
---
+++ example/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/spark4862/workspacetest/mypkg"
+)
+
+func main() {
+	mypkg.Bar()
+}
